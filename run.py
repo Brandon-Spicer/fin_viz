d236efbb3961b97474f17bb0ab9b868063ca188f
Brandon Spicer <brandon.david.spicer@gmail.com>
2024-10-18 05:25:52 -0700

    Get prices from tickers, calculate equity

--- run.py
+++ run.py
@@ -1,17 +1,31 @@
-# import yfinance as yf
-
-# # Define the stock ticker symbol
-# ticker_symbol = "AAPL"  # Replace with the desired stock symbol
-
-# # Get stock data
-# stock = yf.Ticker(ticker_symbol)
-
-# # Get the current stock price
-# current_price = stock.history(period="1d")['Close'][0]
-
-# print(f"Current price of {ticker_symbol} is: ${current_price:.2f}")
-
 import yfinance as yf
+import pandas as pd
+
+# Get portfolio csv and put into dataframe
+
+df = pd.read_csv('portfolio.csv')
+
+# Function to fetch the current stock price using yfinance
+def get_stock_price(ticker):
+    stock = yf.Ticker(ticker)
+    price = stock.history(period="1d")['Close'].iloc[-1]  # Get the latest closing price
+    return price
+
+# Create a new column 'EquityValue' by fetching the stock price and multiplying by share count
+df['Equity'] = df.apply(lambda row: get_stock_price(row['Ticker']) * row['Shares'], axis=1)
+df['Equity'] = round(df['Equity'], 2)
+
+print(df)
+
+s = df['Equity'].sum()
+print(f'Total: ${round(s, 2)}')
+
+
+
+
+
+
+'''
 
 # Define the stock ticker symbol
 ticker_symbol = "AAPL"  # Replace with your desired stock symbol
@@ -25,7 +39,6 @@ print(data['Open'].iloc[0])
 print(data['Close'].iloc[0])
 
 
-'''
 # Get the last row which typically includes the latest available price, including after-hours if the market is closed
 last_price = data['Close'][-1]
 
